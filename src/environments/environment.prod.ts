840088006b315ee7b941f2ce5d87b830fadec26c
Cainan Beltrame Tornich <cainan.developer@gmail.com>
2023-12-29 22:20:07 -0300

    Correção link servidor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    SECRET:'Q0FKU0AyMDIz', // CAJS@2023
-    base_api_url:'https://infinitygroupapi-production.up.railway.app/api/v1',
+    base_api_url:'https://equipealcanceapi-production.up.railway.app/api/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    SECRET:'Q0FKU0AyMDIz', // CAJS@2023
     base_api_url:'http://127.0.0.1:8080/api/v1',
 };
